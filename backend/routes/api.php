94e9eb36ec51ca3a2263b9dc09b8c51f38f3ee99
DNC202 <nguyencongdanh2002@gmail.com>
2024-03-10 20:59:46 +0700

    create category controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,13 +21,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [UserController::class, 'logout']);
+});
 
-Route::prefix('users')->group(function () {
+
+Route::prefix('auth')->group(function () {
     Route::post('/login', [UserController::class, 'login']);
     Route::post('/register', [UserController::class, 'register']);
+    Route::get('/email/verify', function () {
+        return response()->json(['message' => 'Email verified successfully',], 200);
+    })->middleware('auth')->name('verification.notice');
 });
 
 Route::prefix('categories')->group(function () {
     Route::get('/', [CategoryController::class, 'index']);
+    Route::get('/{id}', [CategoryController::class, 'show']);
     Route::post('/create', [CategoryController::class, 'create']);
+    Route::put('/edit/{id}', [CategoryController::class, 'edit']);
+    Route::delete('/delete/{id}', [CategoryController::class, 'delete']);
 });
